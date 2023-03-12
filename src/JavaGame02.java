a28aee42df3ef27b423cbedd86370acf79ba367a
bzhang <bzhang@onespan.com>
2023-03-12 18:21:42 -0400

    Modified JavaGame02 step 3

--- src/JavaGame02.java
+++ src/JavaGame02.java
@@ -3,6 +3,6 @@ class JavaGame01 {
     public static void main(String[] args) {
         System.out.println("Hello1");
         System.out.println("Hello2");
-
+        System.out.println("Hello3");
     }
 }
