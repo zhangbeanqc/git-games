9564b3524425defd910e4228fb5e330ac818ef65
bzhang <bzhang@onespan.com>
2023-03-12 16:03:39 -0400

    All in one step

---
+++ src/JavaGame02.java
@@ -0,0 +1,6 @@
+class JavaGame02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/JavaGame03.java
@@ -0,0 +1,27 @@
+class JavaGame03 {
+
+    public static void main(String[] args) {
+        System.out.println("New Hello 1");
+        System.out.println("New Hello 2");
+        System.out.println("New Hello 3");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+
+        System.out.println("Hello4");
+        System.out.println("Hello5");
+        System.out.println("Hello6");
+
+        System.out.println("Hello10");
+        System.out.println("Hello11");
+        System.out.println("Hello12");
+
+        System.out.println("Hello13");
+        System.out.println("Hello14");
+        System.out.println("Hello15");
+
+        System.out.println("Hello16");
+        System.out.println("Hello17");
+        System.out.println("Hello18");
+    }
+}
