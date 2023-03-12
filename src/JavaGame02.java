553ff942deceaa299d1f6d5c538102e3e279571d
bzhang <bzhang@onespan.com>
2023-03-12 18:22:04 -0400

    Modified JavaGame02 step 5

--- src/JavaGame02.java
+++ src/JavaGame02.java
@@ -5,5 +5,6 @@ class JavaGame01 {
         System.out.println("Hello2");
         System.out.println("Hello3");
         System.out.println("Hello4");
+        System.out.println("Hello5");
     }
 }
