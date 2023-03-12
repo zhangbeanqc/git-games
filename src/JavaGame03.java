c341fc61bc2383cacfd6b12199f683f74d7ae8ad
bzhang <bzhang@onespan.com>
2023-03-12 16:16:00 -0400

    Modified JavaGame03

--- src/JavaGame03.java
+++ src/JavaGame03.java
@@ -1,6 +1,9 @@
 class JavaGame03 {
 
     public static void main(String[] args) {
+        System.out.println("New Hello 1");
+        System.out.println("New Hello 2");
+        System.out.println("New Hello 3");
         System.out.println("Hello1");
         System.out.println("Hello2");
         System.out.println("Hello3");
